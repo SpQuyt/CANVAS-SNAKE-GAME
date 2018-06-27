94878c987146de87dc0b90999490087a31937fc9
SpQuyt <spquyt@gmail.com>
2018-06-27 16:38:55 +0700

    .

--- script.js
+++ script.js
@@ -12,23 +12,23 @@ function component(radius, color, x, y) {
 	this.radius = radius;
 	this.x = x;
 	this.y = y;
-	this.update = function() {
-		ctx = myGameArea.context;		//hinh tron bitch
+	this.update = function() {										//this function is to DRAW.
+		ctx = myGameArea.context;		
 		ctx.beginPath();
 	    ctx.fillStyle = color;
-	    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-	    // ctx.rect(this.x, this.y, this.height, this.width);
+	    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);		//hinh tron bitch
+	    // ctx.rect(this.x, this.y, this.height, this.width);		//hinh tu giac bitch
 	    ctx.fill();
 	}
 }
 
 var myGameArea = {
-	canvas: document.createElement("canvas"),
+	canvas: document.getElementById("mycanvas"),
 	start: function() {
 		this.canvas.width = 480;
 		this.canvas.height = 210;
 		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas,document.body.childNode);
+		// document.body.insertBefore(this.canvas,document.body.childNode);
 	},
 	clear: function() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);			//phai xoa duoc hinh tron
@@ -40,10 +40,11 @@ function updateGameArea(){
 		myGameArea.clear();
 		gamePiece.x += 1;
 		gamePiece.update();
-		if (gamePiece.x == 480 - (gamePiece.radius*2 + 1) || gamePiece.y == 210 - (gamePiece.radius*2 + 1)) {
+		if (gamePiece.x == myGameArea.canvas.width - gamePiece.radius || gamePiece.y == myGameArea.canvas.height - gamePiece.radius) {	//TÂM CỦA NÓ Ở TOẠ ĐỘ (10,120)
 			clearInterval(interval);
 		}
 	},10);
 	
 }
+		
 startGame();
