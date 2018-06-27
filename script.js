61de947546398c2c83c69746248b6055de8fcf3f
Zhang UK <spquyt@gmail.com>
2018-06-27 21:44:43 +0700

    .

--- script.js
+++ script.js
@@ -2,51 +2,119 @@ var gamePiece;
 
 function startGame() {
 	myGameArea.start();
-	gamePiece = new component(10, "red", 10, 120);
-	gamePiece.dx = 1;
+	gamePiece = new List();
+	gamePiece.addToLast(new component(130,100));
+	gamePiece.addToLast(new component(130-10*2-10,100));
+	gamePiece.dx = 30;
 	gamePiece.dy = 0;
 	document.addEventListener("keydown", (event) => {
 			    switch (event.keyCode) {
 			    	case 37:
-			    		gamePiece.dx = -1;
+			    		gamePiece.dx = -30;
 			    		gamePiece.dy = 0;
 			    		break;
 			    	case 38:
 			    		gamePiece.dx = 0;
-			    		gamePiece.dy = -1;
+			    		gamePiece.dy = -30;
 			    		break;
 			    	case 39:
-			    		gamePiece.dx = 1;
+			    		gamePiece.dx = 30;
 			    		gamePiece.dy = 0;
 			    		break;
 			    	case 40:
 			    		gamePiece.dx = 0;
-			    		gamePiece.dy = 1;
+			    		gamePiece.dy = 30;
 			    		break;
 			    }
 			});
 	updateGameArea();
 }
 
-function component(radius, color, x, y) {
+function component(x, y) {
 	// this.width = width;
 	// this.height = height;
-	this.dx;
-	this.dy;
-	this.radius = radius;
-	this.x = x;
-	this.y = y;
-	this.update = function() {										//this function is to DRAW.
-		ctx = myGameArea.context;			
-		ctx.beginPath();											// luon luon phai co begin :D
-	    ctx.fillStyle = color;
-	    ctx.lineWidth = 10;
-	    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);		//hinh tron bitch
-	    // ctx.rect(this.x, this.y, this.height, this.width);		//hinh tu giac bitch
-	    ctx.stroke();												// to vien
-	    ctx.fill();													//to mau
+	// this.dx;
+	// this.dy;
+	this.color = "red";
+	this.radius = 10;
+	this.pos_x = x;
+	this.pos_y = y;
+	this.next = null;
+	ctx = myGameArea.context;			
+	ctx.beginPath();													// luon luon phai co begin :D
+    ctx.fillStyle = this.color;
+    ctx.lineWidth = 10;
+    ctx.arc(this.pos_x, this.pos_y, this.radius, 0, 2 * Math.PI);			//hinh tron bitch
+    // ctx.rect(this.pos_x, this.pos_y, this.height, this.width);		//hinh tu giac bitch
+    ctx.stroke();														// to vien
+    ctx.fill();															//to mau
+}
+
+function List(){
+	this.dx = 0;
+	this.dy = 0;
+	this.head = new component();
+	this.length = 0;
+	this.addToLast = function(anode){
+		curnode = this.head;
+
+		if (curnode.next == null){
+			curnode.next = anode;
+		}
+		else{
+			while(curnode.next != null){
+				curnode = curnode.next;
+			}
+			curnode.next = anode;
+			this.length++;
+		}
+	};
+	this.deleteLast = function(){
+		after = this.head;
+		to_del = this.head;
+		while (to_del.next != null){
+			after = to_del;
+			to_del = to_del.next;
+		}
+		after.next = null;
+		delete to_del;
+	};
+	this.move = function(){
+		temp = new component();
+		temp.pos_x = this.head.next.pos_x + this.dx;
+		temp.pos_y = this.head.next.pos_y + this.dy;
+		temp.next = this.head.next;
+		this.head.next = temp;
+		this.deleteLast();
+		redraw();
+		// if (map[temp.pos_x][temp.pos_y] != '◦'){
+		// 	this.deleteLast();
+		// }
+		// else{
+		// 	this.length++;
+		// 	generate_food()
+		// }
 	}
 }
+
+function redraw() {												//this function is to DRAW.
+		temp = new component();
+		temp.pos_x = gamePiece.head.next.pos_x;
+		temp.pos_y = gamePiece.head.next.pos_y;
+		temp.next = gamePiece.head.next;
+		while (temp != null){
+			ctx = myGameArea.context;			
+			ctx.beginPath();													// luon luon phai co begin :D
+		    ctx.fillStyle = temp.color;
+		    ctx.lineWidth = 10;
+		    ctx.arc(temp.pos_x, temp.pos_y, temp.radius, 0, 2 * Math.PI);			//hinh tron bitch
+		    // ctx.rect(this.pos_x, this.pos_y, this.height, this.width);		//hinh tu giac bitch
+		    ctx.stroke();														// to vien
+		    ctx.fill();															//to mau
+		    temp = temp.next;													
+		}
+		
+	}
 
 var myGameArea = {
 	canvas: document.getElementById("mycanvas"),
@@ -63,14 +131,13 @@ var myGameArea = {
 function updateGameArea(){
 	var interval = setInterval(function() {
 		myGameArea.clear();
-		gamePiece.x += gamePiece.dx;
-		gamePiece.y += gamePiece.dy;
-		gamePiece.update();
-		if (gamePiece.x == myGameArea.canvas.width - gamePiece.radius || gamePiece.y == myGameArea.canvas.height - gamePiece.radius) {	//TÂM CỦA NÓ Ở TOẠ ĐỘ (10,120)
-			clearInterval(interval);
-		}
-	},10);
-	
+		gamePiece.pos_x += gamePiece.dx;
+		gamePiece.pos_y += gamePiece.dy;
+		gamePiece.move();
+		// if (gamePiece.pos_x == myGameArea.canvas.width - gamePiece.radius || gamePiece.pos_y == myGameArea.canvas.height - gamePiece.radius) {	//TÂM CỦA NÓ Ở TOẠ ĐỘ (10,120)
+		// 	clearInterval(interval);
+		// }
+	},100);
 }
 
 startGame();
